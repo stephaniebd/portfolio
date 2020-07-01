f23cc562747e50c841ef59ea99b536118c96d097
Stephanie Beauzile-Domond <stephanie.beauzile.d@gmail.com>
2020-06-30 21:17:19 -0400

    set font and colors variables, install React Bootstrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,10 +12,10 @@ const IndexPage = () => {
     <Layout>
       <SEO title="Home" />
       <h1>Hi.</h1>
-      <h1>I'm Stepahnie Beauzile-Domond</h1>
-      <p>Junior UI/UX & Web designer living in MTL</p>
-      <Link to='/work'>Have a look</Link>
-      <Link to='/contact'>Conact me</Link>
+      <h1>I'm Stephanie Beauzile-Domond</h1>
+      <h2>Junior UI/UX & Web designer living in MTL</h2>
+      <p><Link to='/work'>Have a look</Link></p>
+      <p><Link to='/contact'>Conact me</Link></p>
     </Layout>
   )
 }
